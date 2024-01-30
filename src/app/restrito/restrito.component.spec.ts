10587904337d28fde7815025f6740c57ef041d2a
Jean <jean.fbrito1@gmail.com>
2024-01-30 12:28:14 -0300

    SP3 Angular

--- src/app/restrito/restrito.component.spec.ts
+++ src/app/restrito/restrito.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TarefaComponent } from './tarefa.component';
+import { RestritoComponent } from './restrito.component';
 
-describe('TarefaComponent', () => {
-  let component: TarefaComponent;
-  let fixture: ComponentFixture<TarefaComponent>;
+describe('RestritoComponent', () => {
+  let component: RestritoComponent;
+  let fixture: ComponentFixture<RestritoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TarefaComponent ]
+      declarations: [ RestritoComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(TarefaComponent);
+    fixture = TestBed.createComponent(RestritoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
